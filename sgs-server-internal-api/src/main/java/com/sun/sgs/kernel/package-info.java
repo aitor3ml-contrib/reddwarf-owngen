66d93407a3b4f2b307ab83b3df0f698eba7b4792
aitor3ml <aitor3ml@gmail.com>
2013-03-24 11:03:24 +0000

    sgs from reddwarf with je upgrade to 5.0.73
    git-svn-id: http://svn.dinastyao.com/sgs/trunk@229 33887c4b-bda5-43c3-93ea-48334d814abc

---
+++ sgs-server-internal-api/src/main/java/com/sun/sgs/kernel/package-info.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2010 The RedDwarf Authors.  All rights reserved
+ * Portions of this distribution have been modified as part of RedDwarf
+ * The source code is governed by a GPLv2 license with "Classpath"
+ * exception that can be found in the LICENSE file.
+ */
+/*
+ * Copyright 2007-2010 Sun Microsystems, Inc.
+ *
+ * This file is part of Project Darkstar Server.
+ *
+ * Project Darkstar Server is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation and
+ * distributed hereunder to you.
+ *
+ * Project Darkstar Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Sun designates this particular file as subject to the "Classpath"
+ * exception as provided by Sun in the LICENSE file that accompanied
+ * this code.
+ *
+ * --
+ */
+
+/**
+ * Provides the core system interfaces used by the RedDwarf Server.
+ */
+package com.sun.sgs.kernel;
---
+++ sgs-server/src/main/java/com/sun/sgs/impl/kernel/package-info.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2010 The RedDwarf Authors.  All rights reserved
+ * Portions of this file have been modified as part of RedDwarf
+ * The source code is governed by a GPLv2 license that can be found
+ * in the LICENSE file.
+ */
+/*
+ * Copyright 2007-2010 Sun Microsystems, Inc.
+ *
+ * This file is part of Project Darkstar Server.
+ *
+ * Project Darkstar Server is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation and
+ * distributed hereunder to you.
+ *
+ * Project Darkstar Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --
+ */
+
+/**
+ * Provides an implementation of the RedDwarf Server kernel.
+ */
+package com.sun.sgs.impl.kernel;
